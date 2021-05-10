ac61a5227520780015a812fa3ffc787bde3cb944
Agon Karimani <agonkarimani@gmail.com>
2021-05-10 15:37:43 +0200

    Add Button

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Button from './Button'
+
+export default () => {
+  return (
+    <div>
+      <Button isActive>Active</Button>
+      <Button>Not active</Button>
+    </div>
+  )
+}
