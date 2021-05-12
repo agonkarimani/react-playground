6ed49934a2e43a1f38f66b83d215ea65d3b4aee2
Agon Karimani <agonkarimani@gmail.com>
2021-05-12 15:20:05 +0200

    Add things

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-import * as React from 'react'
-import Button from './Button'
-
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import './App.css'
 export default () => {
+  const [todos, setTodos] = useState([])
   return (
-    <div>
-      <Button isActive>Active</Button>
-      <Button>Not active</Button>
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Add todo:
+          <input name="todo" type="text" />
+        </label>
+        <button>Add</button>
+      </form>
+      <ul>
+        {todos.map(({ text, isDone, id }) => (
+          <li onClick={() => toggleIsDone(id)} key={id}>
+            {text} {isDone && '✅'}
+          </li>
+        ))}
+      </ul>
     </div>
   )
+  function toggleIsDone(id) {
+    const index = todos.findIndex(todo => todo.id === id)
+    const todo = todos[index]
+    setTodos([
+      ...todos.slice(0, index),
+      { ...todo, isDone: !todo.isDone },
+      ...todos.slice(index + 1),
+    ])
+  }
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = event.target
+    const input = form.elements.todo
+    // spread operator `...todos`
+    const newTodo = { text: input.value, isDone: false, id: uuidv4() }
+    setTodos([newTodo, ...todos])
+    form.reset()
+    input.focus()
+  }
 }
---
+++ src/Box.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import './Box.css'
+
+export default () => {
+  const [size, setSize] = useState(100)
+  const style = {
+    width: size + 'px',
+    height: size + 'px',
+  }
+  return (
+    <div className="App">
+      <label>
+        Size:
+        <input value={size} onChange={handleChange} type="range" max="200" />
+      </label>
+      <div style={style} className="Box" />
+    </div>
+  )
+  function handleChange(event) {
+    const input = event.target
+    setSize(input.value)
+  }
+}
